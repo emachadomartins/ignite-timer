a01eacea93205ad6032b04e5531a0744537b25d2
hakuunabatata <emachadomartins1999@gmail.com>
2024-06-13 20:58:00 -0300

    create defaultTheme

--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -20,6 +20,10 @@ const buttonVariants: Record<ButtonVariant, string> = {
 export const ButtonContainer = styled.button<ButtonContainerProps>`
   width: 100px;
   height: 40px;
-  background-color: ${(props: ButtonContainerProps) =>
-    buttonVariants[props.variant]};
+  border-radius: 4px;
+  border: 0;
+  margin: 8px;
+
+  background-color: ${props => props.theme['green-500']};
+  color: ${props => props.theme.white};
 `
